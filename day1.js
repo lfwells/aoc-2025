f6bab5151f6f9935b27ab93811efb50ab8011028
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-12-04 01:54:37 +1100

    incomplete part 2s for day 1 and 3

--- day1.js
+++ day1.js
@@ -3,5 +3,68 @@ import { sum } from "./utils.js";
 
 run(1, (input) => 
 {
+    var part1 = 0;
+    var part2 = 0;
+    var lock = 50;
+    var size = 100;
+    var lines = input.split("\n").map(l => [l.substr(0,1), parseInt(l.substr(1))]);
+
+    for (var line of lines)
+    {
+        let [direction, amount] = line;
+        if (direction == "L") amount *= -1;
+
+        //throwing it all out and going to do it the dumb way...
+        /*
+        let original = lock;
+        let turns = Math.floor(Math.abs((lock+amount) / size));
+        //console.log(lock+amount);
+        //console.log((lock+amount) / size);
+
+        let waszero = false;
+        lock = (lock + amount) % size;
+
+        if (lock == 0)
+        {
+            part1++;
+            if (amount < 0)
+                turns++;
+        }
+
+        if (lock < 0) { lock = size + lock; // 0 case?
+            waszero = true;
+            }
+
+        
+        part2+= turns;
+
+
+        if (lock == 0)
+        {
+        console.log({original, amount, new: lock, part2, turns});
+        }*/
+       while (amount != 0)
+       {
+            let take = (direction == "L") ? -100 : 100;
+            if (amount < 100 && amount >= 0)
+            {
+                take = amount;
+            }
+            if (amount < 0 && amount > -100)
+            {
+                take = amount;
+            }
+            console.log({lock, amount, take});
+            
+            amount -= take;
+            lock += take;
+             if (lock < 0) { lock = size + lock; part2++; }
+             else if (lock >= 100) { lock = lock - size; part2++; }
+             else if (lock == 0) part2++;
     
+            console.log({lock, part2});
+       }
+    }
+
+    console.log({part1, part2});
 });
--- day3.js
+++ day3.js
@@ -3,5 +3,74 @@ import { sum } from "./utils.js";
 
 run(3, (input) => 
 {
+    //nearly a one-liner, but order matters...
+    // let banks = (input.split("\n").map(bank => bank.split("").map(n => parseInt(n)).sort().reverse()).map(sorted => parseInt(sorted[0].toString()+sorted[1].toString())));
     
+    // for the one that should have worked..
+    let part1 = sum(input.split("\n").map(bank => bank.split("").map(n => parseInt(n))).map(twoHighest).map(combineIntoInt));
+
+    let part2 = sum(input.split("\n").map(bank => bank.split("").map(n => parseInt(n))).map(bank => nHighest(bank, 12)).map(combineIntoInt));
+
+    console.log({part1, part2}); 
 });
+
+function twoHighest(bank)
+{
+    let highest = 0;
+    let secondHighest = 0;
+    let highestIndex = 0;
+    for (var i = 0; i < bank.length-1; i++) //minus one here to avoid highest number being found on final digit
+    {
+        if (bank[i] > highest)
+        {
+            highest = bank[i];
+            highestIndex = i;
+        }
+    }
+    for (var i = highestIndex + 1; i < bank.length; i++)
+    {
+        if (bank[i] > secondHighest)
+        {
+            secondHighest = bank[i];
+        }
+    }
+    //console.log({bank: bank.join(""), highest, secondHighest, sort: [...bank].sort().reverse().slice(0, 2), sum: combineIntoInt([highest,secondHighest])});
+    return [highest, secondHighest];
+}
+
+//nope too hard...
+function nHighest(bank, n)
+{
+    if (n == 1) {
+        console.log({bank, m: Math.max(...bank), n});
+        return [Math.max(...bank)];
+    }
+
+    let highest = 0;
+    for (var i = 0; i < bank.length; i++) //minus one here to avoid highest number being found on final digit
+    {
+
+        if (bank[i] > highest)
+        {
+            var recurse = nHighest([...bank.slice(i+1)], n-1);
+            if (recurse == undefined || recurse.length == 0) continue;
+
+            var a = [...bank.slice(0, i), ...recurse];
+            if (i == bank.length -1)
+                a = [bank[i]];
+            console.log({a, c: combineIntoInt(a), bank, n});
+            if (combineIntoInt(a) > highest)
+                highest = bank[i];
+        }
+    }
+    console.log({a});
+    return [highest, ...a];
+}
+
+
+function combineIntoInt(arr)
+{
+    //return parseInt(two[0].toString()+two[1].toString());
+    //to work better with part two:
+    return parseInt(arr.join(""));
+}
