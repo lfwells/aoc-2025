d26a0be361fb986430b165e357a89a9db4a5014c
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-12-04 00:31:11 +1100

    day 2 part 1

--- day2.js
+++ day2.js
@@ -2,5 +2,38 @@ import run from "./boilerplate.js";
 
 run(2, (input) => 
 {
-   
+    let part1 = 0;
+
+   let pairs = input.split(",").map(pair => pair.split("-").map(n => parseInt(n)));
+   for (var pair of pairs)
+   {
+        let [start,end] = pair;
+        for (var i = start; i <= end; i++)
+        {
+            if (isInvalid(i))
+                part1 += i;
+        }
+        //break;
+   }
+
+   console.log({part1});
 });
+
+function isInvalid(n)
+{
+    let s = n.toString();
+    let halfLength = s.length / 2;
+    var allSame = true;
+    for (var i = 0; i < halfLength; i++)
+    {
+        var start = s[i];
+        var end = s[halfLength+i];
+        //console.log({start,end});
+        if (start != end)
+        {
+            allSame = false;
+            break;
+        }
+    }
+    return allSame;
+}
